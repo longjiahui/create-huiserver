c1e6358705efc60a825b67aec6b0c8ecfa661734
Anfo <longjiahui@hotmail.com>
2024-12-19 15:35:34 +0800

    feat: v0.0.3

--- source/pms/src/http/guard/user.ts
+++ source/pms/src/http/guard/user.ts
@@ -1,5 +1,10 @@
 import { decodeUser } from "../../service/jwt"
-import { AuthError, createModule, mergeGuard } from "@anfo/huiserver"
+import {
+  AuthError,
+  createGuard,
+  createModule,
+  mergeGuard,
+} from "@anfo/huiserver"
 import type { Context, GuardFunction, Module, v } from "@anfo/huiserver"
 import { getSignedCookieByContext } from "@anfo/huiserver"
 import type { Next } from "koa"
@@ -7,8 +12,36 @@ import { prisma } from "../../db"
 
 type Role = any
 
-function userGuard(role?: Role) {
-  return async (ctx: Context, next: Next) => {
+// function userGuard(role?: Role) {
+//     return async (ctx: Context, next: Next) => {
+//         const token =
+//             getSignedCookieByContext(ctx, 'token') ||
+//             ctx.request.headers.authorization
+//         if (token) {
+//             const user = decodeUser(token)
+//             ctx.state.user = await prisma.user.findUnique({
+//                 where: { email: user.email },
+//             })
+//             if (!ctx.state.user) {
+//                 throw new AuthError('没找到用户嗷')
+//             }
+//         } else {
+//             throw new AuthError('没找到token嗷')
+//         }
+//         return next()
+//     }
+// }
+declare module "@anfo/huiserver" {
+  interface ApplicationGuard {
+    tryUser: () => GuardFunction
+    user: () => GuardFunction
+    userv: (...rest: Parameters<typeof v>) => GuardFunction
+    tryUserV: (...rest: Parameters<typeof v>) => GuardFunction
+  }
+}
+
+export const guardMod = createModule((app) => {
+  app.guard.tryUser = createGuard(() => async (ctx, next) => {
     const token =
       getSignedCookieByContext(ctx, "token") ||
       ctx.request.headers.authorization
@@ -17,23 +50,19 @@ function userGuard(role?: Role) {
       ctx.state.user = await prisma.user.findUnique({
         where: { email: user.email },
       })
-      if (!ctx.state.user) {
-        throw new AuthError("没找到用户嗷")
-      }
-    } else {
-      throw new AuthError("没找到token嗷")
     }
     return next()
-  }
-}
-declare module "@anfo/huiserver" {
-  interface ApplicationGuard {
-    user: typeof userGuard
-    userv: (...rest: Parameters<typeof v>) => GuardFunction
-  }
-}
-
-export const guardMod = createModule((app) => {
-  app.guard.user = userGuard
-  app.guard.userv = (...rest) => mergeGuard(userGuard(), app.guard.v(...rest))
+  })
+  app.guard.user = () =>
+    mergeGuard(app.guard.tryUser(), (ctx, next) => {
+      if (!ctx.state.user) {
+        throw new AuthError("user token decode failed")
+      } else {
+        return next()
+      }
+    })
+  app.guard.userv = (...rest) =>
+    mergeGuard(app.guard.user(), app.guard.v(...rest))
+  app.guard.tryUserV = (...rest) =>
+    mergeGuard(app.guard.tryUser(), app.guard.v(...rest))
 }, "user guard")
